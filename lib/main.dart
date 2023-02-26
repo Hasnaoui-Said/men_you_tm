d45c81559fd1c9a0dc59607d82da4653e5e3cb2a
Hasnaoui-Said <saidhasnaoui.uce@email.com>
2023-02-26 21:56:06 +0100

    create home page and navigation

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:men_you_tm/src/widgets/cart.page.dart';
+import 'package:men_you_tm/src/widgets/categories.page.dart';
+import 'package:men_you_tm/src/widgets/home.page.dart';
+
+void main() => runApp(const HomeApp());
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomeAppState());
+  }
+}
+
+class HomeAppState extends StatefulWidget {
+  const HomeAppState({super.key});
+
+  @override
+  State<HomeAppState> createState() => _HomeAppStateState();
+}
+
+class _HomeAppStateState extends State<HomeAppState> {
+  int currentPageIndex = 0;
+  final tabs = [
+    const HomePage(),
+    const CategoriesPage(),
+    const CartPage(),
+    const Center(
+      child: Text("Setting"),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[currentPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 20,
+        unselectedFontSize: 17,
+        selectedItemColor: Colors.orange,
+        currentIndex: currentPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Category",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_rounded),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Setting",
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+
+  Widget builds(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home, color: Colors.orange),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(
+              Icons.category,
+              color: Colors.orange,
+            ),
+            label: 'Categories',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(
+              Icons.add_shopping_cart_outlined,
+              color: Colors.orange,
+            ),
+            icon: Icon(
+              Icons.add_shopping_cart_outlined,
+              color: Colors.orange,
+            ),
+            label: 'Cart',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(
+              Icons.more_horiz,
+              color: Colors.orange,
+            ),
+            icon: Icon(
+              Icons.more_horiz,
+              color: Colors.orange,
+            ),
+            label: 'Settings',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        const HomePage(),
+        const CategoriesPage(),
+        const CartPage(),
+        Container(
+          color: Colors.green,
+          alignment: Alignment.center,
+          child: const Text('Page 2'),
+        ),
+      ][currentPageIndex],
+    );
+  }
+}
